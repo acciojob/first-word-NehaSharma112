49e2825c0bd26e78eab7011a8a5d1e942d93fbe7
NehaSharma112 <131150788+NehaSharma112@users.noreply.github.com>
2025-03-22 21:07:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function firstWord(s) {
   // your code here
 	let sp = s.indexOf(" ");
 	if (spaceIndex === -1) {
-        return str;
+        return s;
     }
 
     // Otherwise, return the substring from index 0 to the first space
-    return str.slice(0, spaceIndex);
+    return s.slice(0, spaceIndex);
 }
 
 // Do not change the code below
